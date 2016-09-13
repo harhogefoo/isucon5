cb728f743cb392f65a61cbfc26e5bbbf485090f4
harhogefoo <harimanmon@gmail.com>
2016-09-13 22:25:44 +0900

    Modify comments_of_friendts

--- app.rb
+++ app.rb
@@ -219,11 +219,17 @@ SQL
     # end
 
     # 修正
-    cof_query = 'SELECT * FROM comments c JOIN entries e ON c.entry_id = e.id
-                 WHERE c.user_id IN (?) AND ( e.private = 0 OR e.private = 1 AND (e.user_id = ? OR e.user_id IN (?)))
-                 ORDER BY c.id DESC LIMIT 10'
-    db.xquery(cof_query, friend_ids, current_user[:id], friend_ids).each do |comment|
+    # cof_query = 'SELECT * FROM comments JOIN entries ON comments.entry_id = entries.id
+    #              WHERE c.user_id IN (?) AND ( e.private = 0 OR e.private = 1 AND (e.user_id = ? OR e.user_id IN (?)))
+    #              ORDER BY c.id DESC LIMIT 10'
+    cof_query = 'SELECT comments.*, users.* FROM comments JOIN users ON users.id = comments.user_id ORDER BY comments.id DESC LIMIT 100'
+    db.query(cof_query).each do |comment|
+      entry = db.xquery('SELECT private, user_id, account_name, nick_name FROM entries JOIN users ON users.id = user_id WHERE entries.id = ?', comment[:entry_id]).first
+      entry[:is_private] = (entry[:private] == 1)
+      next if entry[:is_private] && !permitted?(entry[:user_id])
+      comment[:entry] = entry
       comments_of_friends << comment
+      break if comments_of_friends.size >= 10
     end
 
     friends_query = 'SELECT * FROM relations WHERE one = ? OR another = ? ORDER BY created_at DESC'
